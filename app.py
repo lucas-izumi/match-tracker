0844690dff90165c23e295d9571f3e120f70ef1c
Lucas Izumi <lucasizumi@gmail.com>
2025-12-16 23:41:35 -0300

    Add Heroes

--- app.py
+++ app.py
@@ -216,6 +216,17 @@ def stats():
         opponent_stats=opponent_stats
     )
 
+@app.route("/admin/heroes/add", methods=["POST"])
+def add_hero():
+    name = request.form["name"]
+
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("INSERT INTO heroes (name) VALUES (?)", (name,))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("admin_heroes"))
+
 
 # =========================================================
 # START DO SERVIDOR
